b1149fe094fa4e09e3364896f0958f2fb273c79b
Urkul <74872229+Urkul@users.noreply.github.com>
2021-12-14 18:17:04 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
     var floorPath = $(".home-image path"); // значение отдельного этажа в SVG
     var counterUp = $(".counter-up"); // переменная увеличения этажа
     var counterDown = $(".counter-down"); // переменная уменьшения этажа
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
+    var flatsPath = $(".flats path"); // значение отдельной квартиры в модальном окне
+    var flatLink = $(".flat-link"); // значение отдельной квартиры в ссылке
+    var currentFlat = 1; // переменная текущего этажа
 
     // функция наведения мышкой на этаж
     floorPath.on("mouseover", function () {
@@ -10,6 +16,29 @@ $(document).ready(function () {
         floorPath.removeClass("current-floor"); // удаление активного класса у этажей 
         currentFloor = $(this).attr("data-floor"); // получение значения текущего этажа
         $(".counter").text(currentFloor); // запись значения текущего этажа в счетчик
+    });
+
+    floorPath.on("click", toggleModal); // вызов модального окна при клике на этаж
+    modalCloseButton.on("click", toggleModal); // закрытие модального окна при клике на кнопку Х  
+    viewFlatsButton.on("click", toggleModal);
+
+    flatsPath.on("mouseover", function () {
+        currentFlat = $(this).attr("data-flat"); // получение значения текущей квартиры
+        $(`[data-flat-description=${currentFlat}]`).toggleClass("flat-link-active"); // включение подсветки текущей квартиры добавлением класса
+    });
+
+    flatsPath.on("mouseout", function () {
+        $(`[data-flat-description=${currentFlat}]`).toggleClass("flat-link-active"); // выключение подсветки текущей квартиры удалением класса
+    });
+
+    flatLink.on("mouseover", function () {
+        currentFlat = $(this).attr("data-flat-description"); // получение значения текущей квартиры
+        //console.log(currentFlat);
+        $(`[data-flat=${currentFlat}]`).toggleClass("flat-active"); // включение подсветки текущей квартиры добавлением класса
+    });
+
+    flatLink.on("mouseout", function () {
+        $(`[data-flat=${currentFlat}]`).toggleClass("flat-active"); // выключение подсветки текущей квартиры удалением класса
     });
 
     // отслеживание клика по кнопке вверх
@@ -33,4 +62,9 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // включение подсветки текущего этажа добавлением класса 
         }
     });
+
+    // функция добавления и удаления класса открытия закрытия модального окна
+    function toggleModal() {
+        modal.toggleClass('is-open');
+    }
 });
